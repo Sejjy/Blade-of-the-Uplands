0b703644096de0414ecf88bc2e5acdbc7c418d81
Jesse Mirabel <mirabeljessemari@gmail.com>
2024-06-13 21:04:29 +0800

    bug fixes

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -12,6 +12,7 @@ public class Playing extends State implements Statemethods {
     
     private Player player;
     private LevelManager levelManager;
+    private boolean paused;
 
 	public Playing(Game game) {
         super(game);
