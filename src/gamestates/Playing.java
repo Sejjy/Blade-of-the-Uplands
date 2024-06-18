3f47b0f624b19f7b16804089b6d4842423874b4b
Jesse Mirabel <mirabeljessemari@gmail.com>
2024-06-19 01:11:28 +0800

    Personal changes

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -1,40 +1,125 @@
 package gamestates;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import ui.PauseOverlay;
+import utils.LoadSave;
+import static utils.Constants.Environment.*;
 
 public class Playing extends State implements Statemethods {
     
     private Player player;
     private LevelManager levelManager;
-    private boolean paused;
+    private PauseOverlay pauseOverlay;
+    private boolean paused = false;
+
+    private int xLvlOffset;
+    private int leftBorder =  (int)(0.3 * Game.GAME_WIDTH);
+    private int rightBorder = (int)(0.6 * Game.GAME_WIDTH);
+    private int lvlTilesWide = LoadSave.GetLevelData()[0].length;
+    private int maxTilesOffset = lvlTilesWide - Game.TILES_IN_WIDTH;
+    private int maxLvlOffsetX = maxTilesOffset * Game.TILES_SIZE;
+
+    private BufferedImage backgroundImg, bigCloud, medCloud, smallCloud, grassLand, trees;
+    // private int[] grassLandPos;
+    // private Random rnd = new Random();
 
 	public Playing(Game game) {
         super(game);
         initClasses();
+
+        backgroundImg = LoadSave.GetSpriteAtlas(LoadSave.SKY);
+
+        bigCloud = LoadSave.GetSpriteAtlas(LoadSave.BIG_CLOUD);
+        medCloud = LoadSave.GetSpriteAtlas(LoadSave.MED_CLOUD);
+        smallCloud = LoadSave.GetSpriteAtlas(LoadSave.SMALL_CLOUD);
+
+        grassLand = LoadSave.GetSpriteAtlas(LoadSave.LAND);
+        trees = LoadSave.GetSpriteAtlas(LoadSave.TREES);
+
+        // grassLandPos = new int[8];
+        // for(int i = 0; i < grassLandPos.length; i++)
+        //     grassLandPos[i] = (int)(90 * Game.SCALE) + rnd.nextInt((int)(100 * Game.SCALE));
     }
 
     private void initClasses() {
 		levelManager = new LevelManager(game);
 		player = new Player(200, 200, (int) (52 * Game.SCALE), (int) (48 * Game.SCALE));
 		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+        pauseOverlay = new PauseOverlay(this);
 	}
 
     @Override
     public void update() {
-        levelManager.update();
-        player.update();
+        // Freeze when paused
+        if(!paused) {
+            levelManager.update();
+            player.update();
+            checkCloseToBorder();
+        } else {
+            pauseOverlay.update();
+        }
+    }
+
+    private void checkCloseToBorder() {
+        int playerX = (int) player.getHitbox().x;
+        int diff = playerX - xLvlOffset;
+
+        if(diff > rightBorder)
+            xLvlOffset += diff - rightBorder;
+        else if(diff < leftBorder)
+            xLvlOffset += diff - leftBorder;
+
+        if(xLvlOffset > maxLvlOffsetX)
+            xLvlOffset = maxLvlOffsetX;
+        else if(xLvlOffset < 0)
+            xLvlOffset = 0;
     }
 
     @Override
     public void draw(Graphics g) {
-        levelManager.draw(g);
-        player.render(g);
+
+        g.drawImage(backgroundImg, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
+
+        drawBackground(g);
+        
+        levelManager.draw(g, xLvlOffset);
+        player.render(g, xLvlOffset);
+
+        if(paused) {
+            g.setColor(new Color(0, 0, 0, 150));
+            g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
+            pauseOverlay.draw(g);
+        }
+    }
+
+    private void drawBackground(Graphics g) {
+        // CLOUDS
+        for(int i = 0; i < 3; i++) {
+            g.drawImage(bigCloud, i * BIG_CLOUD_WIDTH - (int)(xLvlOffset * 0.1), (int)(28 * Game.SCALE), BIG_CLOUD_WIDTH, BIG_CLOUD_HEIGHT, null);
+        }
+        for(int i = 0; i < 6; i++) {
+            g.drawImage(medCloud, i * MED_CLOUD_WIDTH - (int)(xLvlOffset * 0.15), (int)(28 * Game.SCALE), MED_CLOUD_WIDTH, MED_CLOUD_HEIGHT, null);
+        }
+        for(int i = 0; i < 9; i++) {
+            g.drawImage(smallCloud, i * SMALL_CLOUD_WIDTH - (int)(xLvlOffset * 0.2), (int)(28 * Game.SCALE), SMALL_CLOUD_WIDTH, SMALL_CLOUD_HEIGHT, null);
+        }
+
+        // LANDaddddddd
+        for(int i = 0; i < 9; i++) {
+            g.drawImage(grassLand, i * LAND_WIDTH - (int)(xLvlOffset * 0.3), (int)(90 * Game.SCALE), LAND_WIDTH, LAND_HEIGHT, null);
+        }
+        for(int i = 0; i < 9; i++) {
+            g.drawImage(trees, i * TREE_WIDTH - (int)(xLvlOffset * 0.55), (int)(124 * Game.SCALE), TREE_WIDTH, TREE_HEIGHT, null);
+        }
     }
 
     @Override
@@ -45,17 +130,25 @@ public class Playing extends State implements Statemethods {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
+        if(paused)
+            pauseOverlay.mousePressed(e);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
+        if(paused)
+            pauseOverlay.mouseReleased(e);
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        // TODO Auto-generated method stub
+        if(paused)
+            pauseOverlay.mouseMoved(e);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        if(paused)
+            pauseOverlay.mouseDragged(e);
     }
 
     @Override
@@ -70,8 +163,8 @@ public class Playing extends State implements Statemethods {
         case KeyEvent.VK_SPACE:
             player.setJump(true);
             break;
-        case KeyEvent.VK_BACK_SPACE:
-            Gamestate.state = Gamestate.MENU;
+        case KeyEvent.VK_ESCAPE:
+            paused = !paused;
             break;
         }
     }
@@ -89,6 +182,10 @@ public class Playing extends State implements Statemethods {
             player.setJump(false);
             break;
         }
+    }
+
+    public void unpauseGame() {
+        paused = false;
     }
 
     public void windowFocusLost() {
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -4,14 +4,70 @@ import main.Game;
 
 public class Constants {
 
+	public static class Environment {
+
+		// CLOUDS
+		public static final int BIG_CLOUD_WIDTH_DEFAULT = 576;
+		public static final int BIG_CLOUD_HEIGHT_DEFAULT = 324;
+		
+		public static final int BIG_CLOUD_WIDTH = (int)(BIG_CLOUD_WIDTH_DEFAULT * Game.SCALE);
+		public static final int BIG_CLOUD_HEIGHT = (int)(BIG_CLOUD_HEIGHT_DEFAULT * Game.SCALE);
+
+		public static final int MED_CLOUD_WIDTH_DEFAULT = 576;
+		public static final int MED_CLOUD_HEIGHT_DEFAULT = 324;
+		
+		public static final int MED_CLOUD_WIDTH = (int)(MED_CLOUD_WIDTH_DEFAULT * Game.SCALE);
+		public static final int MED_CLOUD_HEIGHT = (int)(MED_CLOUD_HEIGHT_DEFAULT * Game.SCALE);
+
+		public static final int SMALL_CLOUD_WIDTH_DEFAULT = 576;
+		public static final int SMALL_CLOUD_HEIGHT_DEFAULT = 324;
+		
+		public static final int SMALL_CLOUD_WIDTH = (int)(SMALL_CLOUD_WIDTH_DEFAULT * Game.SCALE);
+		public static final int SMALL_CLOUD_HEIGHT = (int)(SMALL_CLOUD_HEIGHT_DEFAULT * Game.SCALE);
+
+		// GRASSLAND
+		public static final int LAND_WIDTH_DEFAULT = 576;
+		public static final int LAND_HEIGHT_DEFAULT = 324;
+
+		public static final int LAND_WIDTH = (int)(LAND_WIDTH_DEFAULT * Game.SCALE);
+		public static final int LAND_HEIGHT = (int)(LAND_HEIGHT_DEFAULT * Game.SCALE);
+
+		// TREES
+		public static final int TREE_WIDTH_DEFAULT = 576;
+		public static final int TREE_HEIGHT_DEFAULT = 324;
+
+		public static final int TREE_WIDTH = (int)(TREE_WIDTH_DEFAULT * Game.SCALE);
+		public static final int TREE_HEIGHT = (int)(TREE_HEIGHT_DEFAULT * Game.SCALE);
+	}
+
 	public static class UI {
 		public static class Buttons {
 			public static final int B_WIDTH_DEFAULT = 140;
 			public static final int B_HEIGHT_DEFAULT = 56;
+
 			public static final int B_WIDTH = (int) (B_WIDTH_DEFAULT * Game.SCALE);
 			public static final int B_HEIGHT = (int) (B_HEIGHT_DEFAULT * Game.SCALE);
 
 		}
+		public static class PauseButtons {
+			public static final int SOUND_SIZE_DEFAULT = 42;
+			public static final int SOUND_SIZE = (int)(SOUND_SIZE_DEFAULT * Game.SCALE);
+		}
+
+		public static class UrmButtons {
+			public static final int URM_DEFAULT_SIZE = 56;
+			public static final int URM_SIZE = (int)(URM_DEFAULT_SIZE * Game.SCALE);
+		}
+
+		public static class VolumeButtons {
+			public static final int VOLUME_DEFAULT_WIDTH = 28;
+			public static final int VOLUME_DEFAULT_HEIGHT = 44;
+			public static final int SLIDER_DEFAULT_WIDTH = 215;
+
+			public static final int VOLUME_WIDTH = (int)(VOLUME_DEFAULT_WIDTH * Game.SCALE);
+			public static final int VOLUME_HEIGHT = (int)(VOLUME_DEFAULT_HEIGHT * Game.SCALE);
+			public static final int SLIDER_WIDTH = (int)(SLIDER_DEFAULT_WIDTH * Game.SCALE);
+		}	
 	}
 	public static class Directions {
 		public static final int LEFT = 0;
